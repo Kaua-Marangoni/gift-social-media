636b5c2af9743632667b75c759e9e0ff622ce6b9
Kauã Marangoni <kauamarangonitj@gmail.com>
2025-08-29 00:15:20 -0300

    First commit. Finished project

---
+++ script.js
@@ -0,0 +1,26 @@
+// Função de animação de confetes
+const launchConfetti = () => {
+  const duration = 5 * 450;
+  const end = Date.now() + duration;
+  const colors = ['#ff0', '#ff6347', '#7fff00', '#00bfff', '#ff1493'];
+
+  (function frame() {
+    confetti({
+      particleCount: 7,
+      angle: Math.random() * 360,
+      spread: 60,
+      gravity: 1,
+      origin: {
+        x: Math.random(),
+        y: Math.random() - 0.2
+      },
+      colors: [colors[Math.floor(Math.random() * colors.length)]]
+    });
+
+    if (Date.now() < end) {
+      requestAnimationFrame(frame);
+    }
+  })();
+}
+
+window.onload = launchConfetti;
